ab51fa11b658579876a9e894287c4ac21f5374db
pmurph03 <pmurph.software@gmail.com>
2017-11-15 23:37:37 -0500

    Working in 411 now.

---
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "TankAimingComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h" // Put new includes above
+
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+private:
+	// Sets default values for this pawn's properties
+	ATank();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	
+};
